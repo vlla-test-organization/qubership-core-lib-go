e8a22ea5db9235dfaf2d194ab48b72fab88db925
Sergey Lisovoy <lis0x90@gmail.com>
2025-05-13 21:25:54 +0500

    feat: add annotation mapper serviceloading implementation

---
+++ utils/resource_group_annotations_mapper.go
@@ -0,0 +1,34 @@
+package utils
+
+// AnnotationMapper service loading abstraction for annotations that have different group name
+type AnnotationMapper interface {
+	Find(annotations map[string]string, key string) (string, bool)
+	ExtendList(annotations map[string]string) map[string]string
+}
+
+type ResourceGroupAnnotationsMapper struct {
+	groups []string
+}
+
+func NewResourceGroupAnnotationMapper(groups ...string) *ResourceGroupAnnotationsMapper {
+	return &ResourceGroupAnnotationsMapper{groups: groups}
+}
+
+func (g *ResourceGroupAnnotationsMapper) Find(annotations map[string]string, key string) (string, bool) {
+	for _, v := range g.groups {
+		if value, found := annotations[v+"/"+key]; found {
+			return value, true
+		}
+	}
+	return "", false
+}
+
+func (g *ResourceGroupAnnotationsMapper) ExtendList(annotations map[string]string) map[string]string {
+	labeledAnnotations := make(map[string]string)
+	for _, group := range g.groups {
+		for k, v := range annotations {
+			labeledAnnotations[group+"/"+k] = v
+		}
+	}
+	return labeledAnnotations
+}
---
+++ utils/resource_group_annotations_mapper_test.go
@@ -0,0 +1,132 @@
+package utils
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGroupAnnotationGetter_Get(t *testing.T) {
+	tests := []struct {
+		name        string
+		annotations map[string]string
+		groups      []string
+		key         string
+		expectedVal string
+		expectedOk  bool
+	}{
+		{
+			name: "key exists in first group",
+			annotations: map[string]string{
+				"group1/key1": "value1",
+				"group2/key1": "value2",
+			},
+			groups:      []string{"group1", "group2"},
+			key:         "key1",
+			expectedVal: "value1",
+			expectedOk:  true,
+		},
+		{
+			name: "key exists in second group",
+			annotations: map[string]string{
+				"group1/key1": "value1",
+				"group2/key1": "value2",
+			},
+			groups:      []string{"group2", "group1"},
+			key:         "key1",
+			expectedVal: "value2",
+			expectedOk:  true,
+		},
+		{
+			name: "key does not exist in any group",
+			annotations: map[string]string{
+				"group1/key1": "value1",
+				"group2/key1": "value2",
+			},
+			groups:      []string{"group1", "group2"},
+			key:         "key2",
+			expectedVal: "",
+			expectedOk:  false,
+		},
+		{
+			name:        "empty annotations",
+			annotations: map[string]string{},
+			groups:      []string{"group1", "group2"},
+			key:         "key1",
+			expectedVal: "",
+			expectedOk:  false,
+		},
+		{
+			name: "empty groups",
+			annotations: map[string]string{
+				"group1/key1": "value1",
+			},
+			groups:      []string{},
+			key:         "key1",
+			expectedVal: "",
+			expectedOk:  false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			getter := NewResourceGroupAnnotationMapper(tt.groups...)
+			val, ok := getter.Find(tt.annotations, tt.key)
+
+			if val != tt.expectedVal {
+				t.Errorf("expected value %q, got %q", tt.expectedVal, val)
+			}
+			if ok != tt.expectedOk {
+				t.Errorf("expected ok %v, got %v", tt.expectedOk, ok)
+			}
+		})
+	}
+}
+
+func TestGroupAnnotationMapper_Set(t *testing.T) {
+	tests := []struct {
+		name            string
+		initAnnotations map[string]string
+		expAnnotations  map[string]string
+		groups          []string
+	}{
+		{
+			name: "key exists in first group",
+			initAnnotations: map[string]string{
+				"key1": "value1",
+				"key2": "value2",
+			},
+			expAnnotations: map[string]string{
+				"group1/key1": "value1",
+				"group2/key1": "value1",
+				"group1/key2": "value2",
+				"group2/key2": "value2",
+			},
+			groups: []string{"group1", "group2"},
+		},
+		{
+			name:            "empty annotations",
+			initAnnotations: map[string]string{},
+			expAnnotations:  map[string]string{},
+			groups:          []string{"group1", "group2"},
+		},
+		{
+			name: "empty groups",
+			initAnnotations: map[string]string{
+				"key1": "value1",
+			},
+			expAnnotations: map[string]string{},
+			groups:         []string{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			getter := NewResourceGroupAnnotationMapper(tt.groups...)
+			expAnn := getter.ExtendList(tt.initAnnotations)
+			if !reflect.DeepEqual(expAnn, tt.expAnnotations) {
+				t.Errorf("expected annotations %v, got %v", tt.expAnnotations, expAnn)
+			}
+
+		})
+	}
+}
